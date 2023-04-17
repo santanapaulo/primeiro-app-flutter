e000c838d7cfe1f872299fa943c3980bf8716649
Paulo Santana (PH) <santana.pauloh@gmail.com>
2023-04-17 11:10:55 -0300

    adding shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:primeiro_app_flutter/home/presentation/page/home_page.dart';
 import 'package:primeiro_app_flutter/injection.dart';
 import 'package:primeiro_app_flutter/shared/theme/application_theme.dart';
-import 'package:primeiro_app_flutter/shared/theme/data/app_theme.dart';
+import 'package:primeiro_app_flutter/shared/theme/data/cubit/theme_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,13 +22,26 @@ void main() async {
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ApplicationTheme.themes[AppTheme.halloween]?.themeData,
-      home: const HomePage(),
+    return BlocProvider<ThemeCubit>(
+      create: (_) => GetIt.I.get<ThemeCubit>(),
+      child: const _App(),
+    );
+  }
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, String>(
+      builder: (_, state) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ApplicationTheme.byName(state)?.themeData,
+        home: const HomePage(),
+      ),
     );
   }
 }
--- lib/shared/theme/data/app_theme_model.dart
+++ lib/shared/theme/data/app_theme_model.dart
@@ -6,8 +6,10 @@ class AppThemeModel {
   AppThemeModel({
     required this.theme,
     required this.themeData,
+    required this.icon,
   });
 
   final AppTheme theme;
   final ThemeData themeData;
+  final Icon icon;
 }
